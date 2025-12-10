72ff3060ecb2b9bf22d28e504c3dc10abeb4ec5f
iraana <24048851@saultcollege.ca>
2025-12-10 13:17:51 -0500

    changed IP

--- resources/deploy_lambda.sh
+++ resources/deploy_lambda.sh
@@ -33,7 +33,6 @@ zip -r9 ../$ZIP_FILE ./* -x "*.git*" -x "*.DS_Store" > /dev/null
 cd ..
 
 echo "[4/4] Deploying to AWS Lambda..."
-# Replace these two lines with a command that updates your Lambda function code with the new $ZIP_FILE
 aws lambda update-function-code \
     --function-name $LAMBDA_NAME \
     --zip-file fileb://$ZIP_FILE
